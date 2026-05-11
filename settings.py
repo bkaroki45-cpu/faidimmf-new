a79f06a06678102e33912fba60ab1a0922c2eb8f
root <root@vultr.guest>
2026-05-11 13:09:13 +0000

    Clean repo: remove venv and cache files

---
+++ settings.py
@@ -0,0 +1,19 @@
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "file": {
+            "level": "INFO",
+            "class": "logging.FileHandler",
+            "filename": "/var/log/faidimmf/django.log",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["file"],
+            "level": "INFO",
+            "propagate": True,
+        },
+    },
+}
